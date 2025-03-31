293c1080b97495f0524ecceb11a7f85078a795c2
Ayconey <ayconey@gmail.com>
2025-03-31 18:46:48 +0200

    zmrozilo fest

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -88,6 +88,7 @@ CHANNEL_LAYERS = {
 }
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
